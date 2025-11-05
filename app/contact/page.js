a0c5989342a1b491e5a4db12b7c1bd951855b983
Kauê Barreto Benicio <kauebarretobenicio@gmail.com>
2025-11-05 14:21:04 -0300

    Página Contato adicionada

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import styles from './contact.module.css';
+
+//Página de Contato
+export default function ContactPage() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Página de Contato</h1>
+            <p>Bem vindo á página de contato</p>
+        </div>
+    );
+}
